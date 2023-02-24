71e502deba639506e3a382285061309f7f8c0f7b
s1ckret <marked-obsidian@s1ckret.space>
2023-02-24 16:12:53 +0200

    test: add core requirements
    Signed-off-by: s1ckret <marked-obsidian@s1ckret.space>

--- spec/index.test.js
+++ spec/index.test.js
@@ -1,5 +1,5 @@
 import { marked } from 'marked';
-import obsidian from '../src/index.js';
+import { markedObsidian } from '../src/index.js';
 
 describe('obsidian', () => {
   beforeEach(() => {
@@ -7,12 +7,72 @@ describe('obsidian', () => {
   });
 
   test('no options', () => {
-    marked.use(obsidian());
-    expect(marked('example markdown')).toBe('<p>example html</p>\n');
+    marked.use(markedObsidian());
+    expect(marked('example markdown')).toBe('<p>example markdown</p>\n');
   });
 
   test('markdown not using this extension', () => {
-    marked.use(obsidian());
+    marked.use(markedObsidian());
     expect(marked('not example markdown')).not.toBe('<p>example html</p>\n');
+  });
+
+  test('it should parse obsidian links', () => {
+    const options = {
+      links: {
+        'a link': 'a-link.html'
+      }
+    };
+
+    marked.use(markedObsidian(options));
+
+    const html = marked.parse('This is [[a link]].');
+    expect(html).toBe('<p>This is <a href="a-link.html">a link</a>.</p>\n');
+  });
+
+  test('it should parse obsidian links with alt text', () => {
+    const options = {
+      links: {
+        'a link': 'a-link.html'
+      }
+    };
+
+    marked.use(markedObsidian(options));
+
+    const html = marked.parse('This is [[a link|my cat]].');
+    expect(html).toBe('<p>This is <a href="a-link.html">my cat</a>.</p>\n');
+  });
+
+  test('it should parse non-existent obsidian links', () => {
+    const options = {
+      hrefForNonExistentLinks: 'error.html'
+    };
+    marked.use(markedObsidian(options));
+
+    const html = marked.parse('This is [[a link]].');
+    expect(html).toBe('<p>This is <a href="error.html">a link</a>.</p>\n');
+  });
+
+  test('it should parse obsidian images', () => {
+    const options = {
+      links: {
+        'an image.png': 'media/an-image.png'
+      }
+    };
+
+    marked.use(markedObsidian(options));
+
+    const html = marked.parse('This is ![[an image.png]].');
+    expect(html).toBe('<p>This is <img src="media/an-image.png" alt="an image.png">.</p>\n');
+  });
+
+  test('it should parse non-existent obsidian images', () => {
+    const options = {
+      srcForNonExistentImages: 'no-image.png'
+    };
+
+    marked.use(markedObsidian(options));
+
+    const html = marked.parse('This is ![[an image.png]].');
+    expect(html).toBe('<p>This is <img src="no-image.png" alt="an image.png">.</p>\n');
   });
 });
