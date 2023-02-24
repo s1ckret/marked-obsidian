02c64674824ef2a6c66d8090ecd26884f05cfd1b
s1ckret <marked-obsidian@s1ckret.space>
2023-02-24 16:14:08 +0200

    feat: implement parsing obsidian links
    Signed-off-by: s1ckret <marked-obsidian@s1ckret.space>

--- src/index.js
+++ src/index.js
@@ -1,24 +1,70 @@
-export default function(options = {}) {
-  // extension code here
+const defaultOptions = {
+  links: {},
+  hrefForNonExistentLinks: 'non-existent-link.html',
+  srcForNonExistentImages: 'non-existent-image.png'
+};
+
+export function markedObsidian(options) {
+  options = {
+    ...defaultOptions,
+    ...options
+  };
 
   return {
-    tokenizer: {
-      paragraph(src) {
-        if (src !== 'example markdown') {
-          return false;
+    extensions: [
+      {
+        name: 'obsidian',
+        level: 'inline',
+        start(src) {
+          return src.match(/^[^!]?\[\[/)?.index;
+        },
+        tokenizer(src, tokens) {
+          const imageRule = /^!\[\[(.+?)\]\]/;
+          const imageMatch = imageRule.exec(src);
+          if (imageMatch) {
+            const link = imageMatch[1];
+            let src = options.links[link];
+
+            if (!src) {
+              src = options.srcForNonExistentImages;
+            }
+
+            return {
+              type: 'obsidian',
+              raw: imageMatch[0],
+              link,
+              src
+            };
+          }
+
+          const linkRule = /^\[\[(.+?)([|](.+?)?)?\]\]/;
+          const linkMatch = linkRule.exec(src);
+          if (linkMatch) {
+            const link = linkMatch[1];
+            let href = options.links[link];
+
+            if (!href) {
+              href = options.hrefForNonExistentLinks;
+            }
+            const alt = linkMatch[3];
+
+            return {
+              type: 'obsidian',
+              raw: linkMatch[0],
+              link,
+              href,
+              alt
+            };
+          }
+        },
+        renderer(token) {
+          if (token.src) {
+            return `<img src="${token.src}" alt="${token.link}">`;
+          } else {
+            return `<a href="${token.href}">${token.alt ? token.alt : token.link}</a>`;
+          }
         }
-
-        const token = {
-          type: 'paragraph',
-          raw: src,
-          text: 'example html',
-          tokens: []
-        };
-
-        this.lexer.inline(token.text, token.tokens);
-
-        return token;
       }
-    }
+    ]
   };
 }
